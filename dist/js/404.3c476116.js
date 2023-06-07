b0edb8481d534b6c7c159e2b20326689b014ce0f
Ian <ian.brindley@ontrackms.com>
2023-06-07 12:50:27 +0100

    dist

---
+++ dist/js/404.3c476116.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkShootout"]=self["webpackChunkShootout"]||[]).push([[404],{4404:function(n,t,e){e.r(t),e.d(t,{default:function(){return f}});var o=e(3396);const r={class:"score"},s=(0,o._)("h1",null,"This is score page",-1),u=[s];function c(n,t){return(0,o.wg)(),(0,o.iD)("div",r,u)}var a=e(89);const i={},h=(0,a.Z)(i,[["render",c]]);var f=h}}]);
+//# sourceMappingURL=404.3c476116.js.map
---
+++ dist/js/about.b49d9c35.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkShootout"]=self["webpackChunkShootout"]||[]).push([[443],{4007:function(t,n,u){u.r(n),u.d(n,{default:function(){return f}});var o=u(3396);const a={class:"about"},e=(0,o._)("h1",null,"This is an about page",-1),r=[e];function s(t,n){return(0,o.wg)(),(0,o.iD)("div",a,r)}var c=u(89);const i={},h=(0,c.Z)(i,[["render",s]]);var f=h}}]);
+//# sourceMappingURL=about.b49d9c35.js.map
