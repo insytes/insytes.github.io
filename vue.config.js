d0d1d224a3f64c78a50aac4730cad125de20fefe
Ian <ian.brindley@ontrackms.com>
2023-06-07 12:56:52 +0100

    dist

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,9 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/dist/'
+    : '/',
   devServer: {
     open: process.platform === 'darwin',
     host: '0.0.0.0',
